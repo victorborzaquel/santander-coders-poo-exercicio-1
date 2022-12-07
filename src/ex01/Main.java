a8f1aef424432dd18f712f2cec8b8c4abc1c32aa
Luana Sostisso <luanar.sostisso@gmail.com>
2022-12-07 09:52:14 -0300

    exercicio 1

--- src/ex01/Main.java
+++ src/ex01/Main.java
@@ -1,7 +1,10 @@
-package ex01;
+package ExercicioObjPessoa;
 
 public class Main {
     public static void main(String[] args) {
-
+        Pessoa luana = new Pessoa("Luana Sostisso", 18, 1.78);
+        Pessoa victor = new Pessoa ("Victor", 20, 1.81);
+        System.out.println(luana);
+        System.out.println(victor);
     }
 }
--- src/ex01/Pessoa.java
+++ src/ex01/Pessoa.java
@@ -1,4 +1,21 @@
-package ex01;
+package ExercicioObjPessoa;
 
 public class Pessoa {
+    private String nomePessoa;
+    private int idade;
+    private double altura;
+
+    public Pessoa(String nomePessoa, int idade, double altura) {
+        this.nomePessoa = nomePessoa;
+        this.idade = idade;
+        this.altura = altura;
+    }
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "nome:'" + nomePessoa + '\'' +
+                ", idade:'" + idade + '\'' +
+                ", altura:'" + altura + '\'' +
+                '}';
+    }
 }
