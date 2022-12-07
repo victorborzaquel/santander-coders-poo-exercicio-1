a16478705c01754698dba73d501ccefef1f953f0
thcesarc <34942014+thcesarc@users.noreply.github.com>
2022-12-07 15:32:05 +0000

    Implementação da classe Calculadora

--- src/ex02/Calculadora.java
+++ src/ex02/Calculadora.java
@@ -1,4 +1,27 @@
 package ex02;
 
 public class Calculadora {
+    private Double valor1;
+    private Double valor2;
+
+    public Calculadora(Double valor1, Double valor2) {
+        this.valor1 = valor1;
+        this.valor2 = valor2;
+    }
+
+    public Double somar()
+    {
+        return valor1 + valor2;
+    }
+    public Double subtrair()
+    {
+        return valor1 - valor2;
+    }
+    public Double multiplicar()
+    {
+        return valor1 * valor2;
+    }
+    public Double dividir() {
+        return valor1 / valor2;
+    }
 }
--- src/ex02/Main.java
+++ src/ex02/Main.java
@@ -1,7 +1,10 @@
 package ex02;
 
-public class Main {
-    public static void main(String[] args) {
-
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+        Calculadora calculadora = new Calculadora(999.9 , 0);
+        System.out.println(calculadora.dividir());
     }
 }
