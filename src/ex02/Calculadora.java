65578f4402b3355c91a9caa9dbbe73321566a609
Victor Borzaquel <victorborzaquel@outlook.com>
2022-12-07 21:01:04 -0300

    Calculadora Add: toString, Getters and Setters

--- src/ex02/Calculadora.java
+++ src/ex02/Calculadora.java
@@ -24,4 +24,37 @@ public class Calculadora {
     public Double dividir() {
         return valor1 / valor2;
     }
+
+    public Double getValor1()
+    {
+        return valor1;
+    }
+
+    public void setValor1(Double valor1)
+    {
+        this.valor1 = valor1;
+    }
+
+    public Double getValor2()
+    {
+        return valor2;
+    }
+
+    public void setValor2(Double valor2)
+    {
+        this.valor2 = valor2;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "-----Calculadora-----" + "\n" +
+                "Valor 1: " + valor1 + "\n" +
+                "Valor 2: " + valor2 + "\n" +
+                "Soma: " + somar() + "\n" +
+                "Subtração: " + subtrair() + "\n" +
+                "Multiplicação: " + multiplicar() + "\n" +
+                "Divisão: " + dividir() + "\n" +
+                "----------------------";
+    }
 }
--- src/ex02/Main.java
+++ src/ex02/Main.java
@@ -4,7 +4,18 @@ public class Main
 {
     public static void main(String[] args) 
     {
-        Calculadora calculadora = new Calculadora(999.9 , 0);
+        Calculadora calculadora = new Calculadora(999.9 , 0.0);
+
         System.out.println(calculadora.dividir());
+        System.out.println(calculadora.somar());
+        System.out.println(calculadora.subtrair());
+        System.out.println(calculadora.multiplicar());
+
+        calculadora.setValor1(20.0);
+        calculadora.setValor2(10.0);
+
+        System.out.println(calculadora);
+        System.out.println("Valor 1 = " + calculadora.getValor1());
+        System.out.println("Valor 2 = " + calculadora.getValor2());
     }
 }
